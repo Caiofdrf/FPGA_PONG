2f6f91dc807a2a0ea952d111c8f698b7a6efc09d
Caiofdrf <caiodiniz@alu.ufc.br>
2026-06-25 11:36:34 -0300

    add runner

---
+++ run/run.sh
@@ -0,0 +1,10 @@
+xrun -64bit -sv \
+../source/pong_top.sv \
+../source/processing_unit.sv \
+../source/RAM.sv \
+../source/uart_baudgen.sv \
+../source/uart_rx.sv \
+../source/uart_tx.sv \
+../source/uart_top.sv \
+../tb/pong_tb.sv \
+-access +rwc
